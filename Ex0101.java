7179afac276432bd162c5f502a3458230bd3344b
alcieres <alcieres@gmail.com>
2018-04-09 20:35:30 -0300

    Criei o projeto java

---
+++ Ex0101.java
@@ -0,0 +1,20 @@
+package ex0101;
+ 
+import java.util.Scanner;
+
+public class Ex0101 {
+     
+    public static int maior(int a, int b){
+        return a > b ? a : b;
+    }   
+    
+    public static void main(String[] args) {
+      Scanner in = new Scanner (System.in);
+      System.out.println("Digite o primeiro numero:");
+      int a = in.nextInt();
+      System.out.println("Digite o segundo numero:");
+      int b = in.nextInt();
+        System.out.println("O maior número é: " + maior(a, b));
+    }
+         
+}
